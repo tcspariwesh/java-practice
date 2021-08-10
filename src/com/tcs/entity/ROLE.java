ac3529de512cd4f75b48f0496f095d3f012bdb78
ibmpariwesh <ibmpariwesh@gmail.com>
2021-08-10 14:36:06 +0530

    static, enum

---
+++ src/com/tcs/entity/ROLE.java
@@ -0,0 +1,5 @@
+package com.tcs.entity;
+
+public enum ROLE {
+	BEGINNER, INTERMEDIATE, EXPERT
+}
--- src/com/tcs/entity/User.java
+++ src/com/tcs/entity/User.java
@@ -3,6 +3,8 @@ package com.tcs.entity;
 public class User {
 	private String name;
 	private Long mobileNumber;
+	//beginner, intermediate, export
+	private ROLE role;
 	private byte age; // data encapsulation
 	//constructor
 	
