ee21421c391d6a2f3d0e85ecf15eed0b3e376ce0
ibmpariwesh <ibmpariwesh@gmail.com>
2021-08-10 15:25:28 +0530

    polymorphism

--- src/com/tcs/entity/MethodOverloading.java
+++ src/com/tcs/entity/MethodOverloading.java
@@ -2,14 +2,14 @@ package com.tcs.entity;
 
 public class MethodOverloading {
 	public static void main(String[] args) {
-		User1 user1 = new User1();
+		User1 user1 = new Employee1();
 		user1.work();
 		user1.work(4);
 		user1.work("4");
 	}
 }
 
-class User1 {
+class User1 {//super class
 	public void work() {
 		System.out.println("user is working");
 	}
@@ -19,6 +19,18 @@ class User1 {
 	}
 
 	public void work(String duration) {
+		System.out.println("user is working for " + duration + " hours");
+	}
+}
+
+class Employee1 extends User1{ //IS-A relationship = inheritance, sub-class
+	@Override
+	public void work() {
+		super.work();
+		System.out.println("Employee is working");
+	}
+//	@Override
+	public void work(boolean duration) { //overload, not override
 		System.out.println("user is working for " + duration + " hours");
 	}
 }
