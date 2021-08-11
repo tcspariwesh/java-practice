f1ab68024aa450804c6a1a34be498c6a46940994
ibmpariwesh <ibmpariwesh@gmail.com>
2021-08-11 15:20:34 +0530

    multi catch

--- src/com/tcs/exceptionsdemo/ExceptionsDemo.java
+++ src/com/tcs/exceptionsdemo/ExceptionsDemo.java
@@ -4,19 +4,24 @@ public class ExceptionsDemo {
 	public static void main(String[] args) {
 		try {
 			demo2();
+		} catch (InvalidInputException e) {
+			// TODO: handle exception
 		} catch (MyCheckedException e) {
 			e.printStackTrace();
+		} finally {
+			System.out.println("always called");
 		}
-		//		demo1();
+
+		// demo1();
 	}
 
-	private static void demo2() throws MyCheckedException {
+	private static void demo2() throws MyCheckedException, InvalidInputException {
 		throw new MyCheckedException();
 	}
 
 	private static void demo1() {
 		try {
-			System.out.println(1/0);
+			System.out.println(1 / 0);
 		} catch (ArithmeticException ex) {
 			ex.printStackTrace();
 			System.out.println("division by zero is not allowed.");
